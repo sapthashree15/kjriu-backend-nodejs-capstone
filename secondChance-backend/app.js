35d1f27cfaf2b9d707927fa0cda5af31d226ab44
sapthashree15 <kssapthashree@gmail.com>
2024-07-18 06:24:15 -0400

    changes made from theia lab environment

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -34,10 +34,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/secondchance/search', searchRoutes)
 
 // Global Error Handler
-app.use((err, req, res) => {
-  console.error(err)
-  res.status(500).send('Internal Server Error')
-})
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+});
+
 
 app.get('/', (req, res) => {
   res.send('Inside the server')
